79dc497b2c3873bd4b79df72e792d1978fa1f9e5
KeerthanGopi <keerthan44@gmail.com>
2021-09-18 21:22:19 +0530

    Header Migrated

--- App.js
+++ App.js
@@ -1,48 +1,12 @@
 import * as React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { Octicons } from '@expo/vector-icons';
-import { NavigationAction } from "@react-navigation/routers";
+import { View } from 'react-native';
 
-import HomeScreen from "./screens/Home";
-import ProfileScreen from "./screens/Profile";
-import Color from './constants/color'
-import Text from './constants/text'
-import { Button, View } from "react-native";
-
-const Drawer = createDrawerNavigator();
-
-const MyTheme = {
-  dark: true,
-  colors: {
-    primary: Color.activeTint,
-    background: Color.theme,
-    text: Text.primary,
-    notification: Color.theme,
-    card: Color.theme,
-    border: 'white',
-  },
-};
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <NavigationContainer theme={MyTheme}>
-      <Drawer.Navigator
-        drawerType="front"
-        backBehavior="history"
-        initialRouteName='Home'
-        screenOptions={ (props) => ({
-          activeTintColor: "#e91e63",
-          itemStyle: { marginVertical: 10 },
-          headerLeft: () => <Octicons name="three-bars" size={24} color="white" style={{paddingLeft: '4%', paddingRight: '2%'}} onPress={() => props.navigation.toggleDrawer()} />
-        })}
-      >
-        
-        <Drawer.Screen  name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Profile" component={ProfileScreen} />
-      </Drawer.Navigator>
-      <StatusBar style="light"/>
-    </NavigationContainer>
+    <View style={{flex: 1}}>
+      <Header />
+    </View>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import { createDrawerNavigator } from '../node_modules/@react-navigation/drawer';
-import { NavigationContainer } from '../node_modules/@react-navigation/native';
+import * as React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { Octicons } from '@expo/vector-icons';
 
+import HomeScreen from "../screens/Home";
+import ProfileScreen from "../screens/Profile";
+import Color from '../constants/color'
+import Text from '../constants/text'
 
 const Drawer = createDrawerNavigator();
 
+const MyTheme = {
+  dark: true,
+  colors: {
+    primary: Color.activeTint,
+    background: Color.theme,
+    text: Text.primary,
+    notification: Color.theme,
+    card: Color.theme,
+  },
+};
+
 export default function Header() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator 
+    <NavigationContainer theme={MyTheme}>
+      <Drawer.Navigator
         drawerType="front"
-        initialRouteName="Profile"
-        drawerContentOptions={{
-          activeTintColor: '#e91e63',
+        backBehavior="history"
+        initialRouteName='Home'
+        screenOptions={ (props) => ({
+          activeTintColor: "#e91e63",
           itemStyle: { marginVertical: 10 },
-        }}
-       
+          headerLeft: () => <Octicons name="three-bars" size={24} color="white" style={{paddingLeft: '4%', paddingRight: '2%'}} onPress={() => props.navigation.toggleDrawer()} />
+        })}
       >
-       
+        
+        <Drawer.Screen  name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Profile" component={ProfileScreen} />
       </Drawer.Navigator>
+      <StatusBar style="light"/>
     </NavigationContainer>
   );
 }
